7576bde708179ec160b0e98ca0a33dc1fe3c71c5
Apple <apple@Apples-MacBook-Pro-3.local>
2015-09-30 15:00:22 -0700

    updated files if/else

--- game.js
+++ game.js
@@ -1,29 +1,25 @@
-var question1 = prompt('What frightens you?');
-var question2 = prompt('Do you believe in Haunted Houses?');
-var question3 = prompt('Do you avoid watching scary movies and tv shows?');
+// console.log('Question1 response: ' + question1);
+// console.log('Question2 response: ' + question2);
+// console.log('Question3 response: ' + question3);
 
-console.log('Question1 response: ' + question1);
-console.log('Question2 response: ' + question2);
-console.log('Question3 response: ' + question3);
-
-alert('Your answer to Question 1 was ' + question1 + ' ...You are a Halloweenie');
-alert('Your answer to Question 2 was ' + question2 + ' ...You are a Halloweenie');
-alert('Your answer to Question 3 was ' + question3 + ' ...You are a Halloweenie');
-
-
-var question1 = prompt("Was it easy to get to CF today?");
-if (question1.toUpperCase () === 'YES'|| condition2) {
-  alert('Lucky you');
+var question1 = prompt("Are you afraid of the dark?");
+if (question1 === 'yes') {
+  alert('You are a Halloweenie');
 } else {
-  alert{'Sucks to be you'};
+  alert('Hmmm... this is going to be harder than I thought');
 }
 
-keep count of how many times the got it right
+var question2 = prompt("Do you believe in Haunted Houses?");
+if (question2 === 'yes') {
+  alert ('You are a giant Halloweenie');
+} else {
+  alert('You are a tough one to scare!');
+}
 
-console.log('Question1 response: ' + question1);
-console.log(Question 1 response toUpperCase(): ' + question1.toUpperCase());
+var question3 = prompt("Do you avoid watching scary movies and tv shows?");
+if (question3 === 'yes') {
+  alert('You are a mini Halloweenie');
+} else {
+  alert('You are ready for Hallowen!')
+}
 
-alert('Your answer to Question 1 was ' + question1);
-
-summary message (You have gotten how many correct)
-(thank you for playing)
