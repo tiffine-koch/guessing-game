09de33622b49166bf50d7a6471f098eb8565362d
Apple <apple@Apples-MacBook-Pro-3.local>
2015-09-30 14:18:10 -0700

    This is the inital upload

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+this is my readme file
+=======
+# guessing-game
+cf-homework 093015
+>>>>>>> 7857c3097554461b86f82df36641e9d05d4c363e
---
+++ game.js
@@ -0,0 +1,29 @@
+var question1 = prompt('What frightens you?');
+var question2 = prompt('Do you believe in Haunted Houses?');
+var question3 = prompt('Do you avoid watching scary movies and tv shows?');
+
+console.log('Question1 response: ' + question1);
+console.log('Question2 response: ' + question2);
+console.log('Question3 response: ' + question3);
+
+alert('Your answer to Question 1 was ' + question1 + ' ...You are a Halloweenie');
+alert('Your answer to Question 2 was ' + question2 + ' ...You are a Halloweenie');
+alert('Your answer to Question 3 was ' + question3 + ' ...You are a Halloweenie');
+
+
+var question1 = prompt("Was it easy to get to CF today?");
+if (question1.toUpperCase () === 'YES'|| condition2) {
+  alert('Lucky you');
+} else {
+  alert{'Sucks to be you'};
+}
+
+keep count of how many times the got it right
+
+console.log('Question1 response: ' + question1);
+console.log(Question 1 response toUpperCase(): ' + question1.toUpperCase());
+
+alert('Your answer to Question 1 was ' + question1);
+
+summary message (You have gotten how many correct)
+(thank you for playing)
