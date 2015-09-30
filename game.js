d4851adfbf1d78adb396cc1518e0d4d76515af26
Apple <apple@Apples-MacBook-Pro-3.local>
2015-09-30 15:25:14 -0700

    Update file: error correction

--- game.js
+++ game.js
@@ -1,25 +1,24 @@
-// console.log('Question1 response: ' + question1);
-// console.log('Question2 response: ' + question2);
-// console.log('Question3 response: ' + question3);
-
 var question1 = prompt("Are you afraid of the dark?");
-if (question1 === 'yes') {
+if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
   alert('You are a Halloweenie');
 } else {
   alert('Hmmm... this is going to be harder than I thought');
 }
 
 var question2 = prompt("Do you believe in Haunted Houses?");
-if (question2 === 'yes') {
+if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
   alert ('You are a giant Halloweenie');
 } else {
   alert('You are a tough one to scare!');
 }
 
 var question3 = prompt("Do you avoid watching scary movies and tv shows?");
-if (question3 === 'yes') {
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() ==='Y') {
   alert('You are a mini Halloweenie');
 } else {
-  alert('You are ready for Hallowen!')
+  alert('You are ready for Hallowen!');
 }
 
+// console.log('Question1 response: ' + question1);
+// console.log('Question2 response: ' + question2);
+// console.log('Question3 response: ' + question3);
