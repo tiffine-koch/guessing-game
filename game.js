c78675d1ef2d3e57e07f45c103fdb6933b421502
David Swaintek <dswaintek@gmail.com>
2015-10-01 13:01:33 -0700

    Moved alerts to <p> elements

--- game.js
+++ game.js
@@ -1,32 +1,36 @@
 var correct = 0;
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
 
-function ques1(){
+function ques1() {
 	var question1 = prompt("Are you afraid of the dark?");
 	if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
-	  alert('You are a Halloweenie');
+	  one.innerHTML = 'You are a Halloweenie';
 	  correct++;
 	} else {
-	  alert('Hmmm... this is going to be harder than I thought');
+	  one.innerHTML = 'Hmmm... this is going to be harder than I thought';
 	}
 }
 
 function ques2(){
 	var question2 = prompt("Do you believe in Haunted Houses?");
 	if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
-	  alert('You are a giant Halloweenie');
+	  two.innerHTML = 'You are a giant Halloweenie';
 	  correct++;
 	} else {
-	  alert('You are a tough one to scare!');
+	  two.innerHTML = 'You are a tough one to scare!';
 	}
 }
 
 function ques3(){
 	var question3 = prompt("Do you avoid watching scary movies and tv shows?");
 	if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
-	  alert('You are a mini Halloweenie');
+	  three.innerHTML = 'You are a mini Halloweenie';
 	  correct++;
 	} else {
-	  alert('You are ready for Hallowen!');
+	  three.innerHTML = 'You are ready for Hallowen!';
+	}
 }
 
 ques1();
