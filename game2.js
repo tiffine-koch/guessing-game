17e4b4d2944ce0106e9b3835a8c1c1c57950608a
Apple <apple@Apples-MacBook-Pro-3.local>
2015-10-01 10:59:17 -0700

    game updated

--- game2.js
+++ game2.js
@@ -1,10 +1,10 @@
-var YES;
-YES = 1;
+var yes;
+yes = 0;
 
 var question1 = prompt("Are you afraid of the dark?");
 if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
   alert('You are a Halloweenie');
-  YES++;
+  yes++;
 } else {
   alert('Hmmm... this is going to be harder than I thought');
 }
@@ -12,24 +12,24 @@ if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
 var question2 = prompt("Do you believe in Haunted Houses?");
 if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
   alert('You are a giant Halloweenie');
-  YES++;
+  yes++;
 } else {
   alert('You are a tough one to scare!');
 }
 
 var question3 = prompt("Do you avoid watching scary movies and tv shows?");
-if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
+if (question3.toUpperCase() === 'YES' || question3.toUpperCase() ==='Y') {
   alert('You are a mini Halloweenie');
-  YES++;
+  yes++;
 } else {
   alert('You are ready for Hallowen!');
 }
 
-var yes = alert('You are finished! With Halloween right around the corner, we need to check how prepared you are based on your answers.');
+var yes = prompt('You are finished!');
 if (yes >= 2) {
   alert('You were afraid of 2 scary scenarios... sleep with the lights on!');
 } else {
-  alert('You were only afraid of 1 creepy thing... good job!');
+  alert('You were only afraid on one creepy thing... good job!');
 }
 
 
